5cca9947eba4f7a5a2760f69a79d1bec3a3af960
auqid <auqidirfan100@gmail.com>
2025-06-13 16:06:59 +0530

    exercise 2.11

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 import Person from "./components/Person";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
