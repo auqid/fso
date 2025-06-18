c9b0cf36ad6cfd26c38a32ccd0758bb242e89f19
auqid <auqidirfan100@gmail.com>
2025-06-18 18:32:37 +0530

    exercise 3.7-3.8

--- part 3/index.js
+++ part 3/index.js
@@ -27,6 +27,12 @@ let persons = [
 
 app.use(express.json());
 
+morgan.token("body", (request, response) => {
+  return request.method === "POST" ? JSON.stringify(request.body) : "";
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 // get all persons
 app.get("/api/persons", (request, response) => {
   response.json(persons);
