5c775ec657360e080ba74f2516d46b0b040955e5
auqid <auqidirfan100@gmail.com>
2025-06-22 15:59:00 +0530

    exercise 3.12

---
+++ part 3/phonebook/backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+const passwordOnly = process.argv.length === 3;
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+if (!passwordOnly && (!name || !number)) {
+  console.log("Please provide both name and number");
+  process.exit(1);
+}
+const url = `mongodb+srv://auqidirfan100:${password}@cluster0-test.icgy7b8.mongodb.net/phoneBook?retryWrites=true&w=majority&appName=Cluster0-test`;
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: name,
+  number: number,
+});
+
+if (passwordOnly) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((x) => {
+      console.log(x.name + " " + x.number);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  person.save().then((result) => {
+    console.log(`added ${name} ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
