7501f65d7b0b356498707d9c16c5a03f505f1dda
auqid <auqidirfan100@gmail.com>
2025-07-05 22:21:17 +0530

    4.15: Blog List Expansion, step 3

---
+++ part 4/blog list/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: String,
+  passwordHash: String,
+});
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject._v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
