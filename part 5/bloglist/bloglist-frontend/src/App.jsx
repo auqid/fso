7d8510e62f7a9e4a55972248e1f86d662076c7b5
auqid <auqidirfan100@gmail.com>
2025-07-08 15:12:30 +0530

    Convert bloglist-frontend from submodule to regular folder

---
+++ part 5/bloglist/bloglist-frontend/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  console.log(password, username);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setError("Invalid Credentials");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        Username
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <br />
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+  const blogList = () => (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+
+  return (
+    <>
+      {error}
+      {user === null ? loginForm() : blogList()}
+    </>
+  );
+};
+
+export default App;
---
+++ part 5/bloglist/bloglist-frontend/src/services/login.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const baseUrl = "/api/login";
+const login = async (values) => {
+  const response = await axios.post(baseUrl, values);
+  return response.data;
+};
+
+export default { login };
